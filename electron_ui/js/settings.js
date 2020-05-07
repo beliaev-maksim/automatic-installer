98346e06430052097458e0d94fb0a4f78142f817
Maksim Beliaev <beliaev.m.s@gmail.com>
2020-05-07 12:21:42 +0200

    1. moved tables handler to separate JS 2. fixed scroll for HPC table 3. refactor tables functions in see.1

--- electron_ui/js/settings.js
+++ electron_ui/js/settings.js
@@ -48,61 +48,10 @@ $(document).ready(function() {
         "scrollCollapse": true,
         "paging": false,
         "filter": false,
-        "info": false
+        "info": false,
+        "order": [[ 0, "desc" ]]
     } );
 } );
-
-function add_hpc_files_rows() {
-    var table_container = document.getElementById("hpc-options-table");
-    var table = table_container.getElementsByTagName("tbody")[0];
-    $('#hpc-options-table tbody').empty();
-
-    var files_list = fs.readdirSync(hpc_options_folder);
-    files_list = files_list.filter(name => name.includes(".acf"));
-    files_list.sort(function (a, b) {
-        if (a.slice(0, 3) > b.slice(0, 3)) {return -1;}
-        else if (b.slice(0, 3) > a.slice(0, 3)) {return 1;}
-        return 0;
-    });
-
-    for (var i in files_list) {
-
-        var row = table.insertRow(0); // after header
-        var cell_product = row.insertCell(0);
-        cell_product.innerHTML = os_path.join("%APPDATA%", "build_downloader", "HPC_Options", files_list[i]);
-        cell_product.classList.add("tg-gzer");
-        cell_product.classList.add("file-column");
-
-        var createClickHandler = function(selected_row) {
-                return function() {
-                                        var file_name = selected_row.getElementsByTagName("td")[0].innerHTML;
-                                        answer = dialog.showMessageBoxSync(remote.getCurrentWindow(), {
-                                                type: "question",
-                                                buttons: ["Yes", "No"],
-                                                message: "Do you want permanently delete " + file_name + "?"
-                                            }
-                                        )
-
-                                        if (answer == 0) {
-                                            fs.unlinkSync(file_name);
-                                            add_hpc_files_rows();
-                                        }
-                                 };
-        };
-
-        row.onclick = createClickHandler(row);
-    }
-
-    if (files_list.length < 4) {
-        for (i = 0; i < 4 - files_list.length; i++) {
-            var row = table.insertRow(-1); // at the end
-            var cell_product = row.insertCell(0);
-            cell_product.innerHTML = "";
-            cell_product.classList.add("tg-gzer");
-            cell_product.classList.add("file-column");
-        }
-    }
-}
 
 $("#install_path, #download_path, #password, #force_install, #delete_zip, #wb_flags").bind("change", save_settings);
 
---
+++ electron_ui/js/tables_setter.js
@@ -0,0 +1,93 @@
+
+function add_task_rows(tasks_list) {
+    var table_container = document.getElementById("schedule-table");
+    var table = table_container.getElementsByTagName("tbody")[0];
+    $('#schedule-table tbody').empty();
+
+    for (var i in tasks_list) {
+        var row = table.insertRow(0); // after header
+        set_table_row(row, ["product-column", "version-column", "schedule-column"],
+                        [tasks_list[i].product, tasks_list[i].version, tasks_list[i].schedule_time])
+
+        var createClickHandler = function(selected_row) {
+                return function() {
+                                        product = selected_row.getElementsByTagName("td")[0].innerHTML;
+                                        version = selected_row.getElementsByTagName("td")[1].innerHTML;
+                                        answer = dialog.showMessageBoxSync(remote.getCurrentWindow(), {
+                                                type: "question",
+                                                buttons: ["Yes", "No"],
+                                                message: "Do you want to delete scheduled task " + product +
+                                                           " "+ version + "?"
+                                            }
+                                        )
+
+                                        if (answer == 0) {
+                                            pyshell.send('delete_task ' + product  + "_" + version);
+                                            pyshell.send('get_active_tasks');
+                                        }
+
+                                 };
+        };
+
+        row.onclick = createClickHandler(row);
+    }
+
+    if (tasks_list.length < 3) {
+        for (i = 0; i < 3 - tasks_list.length; i++) {
+            var row = table.insertRow(-1); // at the end
+            set_table_row(row, ["product-column", "version-column", "schedule-column"], ["", "", ""])
+        }
+    }
+}
+
+
+function add_hpc_files_rows() {
+    var table_container = document.getElementById("hpc-options-table");
+    var table = table_container.getElementsByTagName("tbody")[0];
+    $('#hpc-options-table tbody').empty();
+
+    var files_list = fs.readdirSync(hpc_options_folder);
+    files_list = files_list.filter(name => name.includes(".acf"));
+
+    for (var i in files_list) {
+        var row = table.insertRow(0); // after header
+        var file_shorten_path = os_path.join("%APPDATA%", "build_downloader", "HPC_Options", files_list[i]);
+        set_table_row(row, ["file-column"], [file_shorten_path], main_class="tg-gzer")
+
+        var createClickHandler = function(selected_row) {
+                return function() {
+                                        var file_name = selected_row.getElementsByTagName("td")[0].innerHTML;
+                                        answer = dialog.showMessageBoxSync(remote.getCurrentWindow(), {
+                                                type: "question",
+                                                buttons: ["Yes", "No"],
+                                                message: "Do you want permanently delete " + file_name + "?"
+                                            }
+                                        )
+
+                                        if (answer == 0) {
+                                            fs.unlinkSync(file_name.replace('%APPDATA%', process.env.APPDATA));
+                                            add_hpc_files_rows();
+                                        }
+                                 };
+        };
+        row.onclick = createClickHandler(row);
+    }
+
+    if (files_list.length < 4) {
+        for (i = 0; i < 4 - files_list.length; i++) {
+            var row = table.insertRow(-1); // at the end
+            set_table_row(row, ["file-column"], [""], main_class="tg-gzer")
+        }
+
+    }
+}
+
+
+function set_table_row(row, class_list, inner_html_list, main_class="tg-baqh"){
+    for (var i in class_list) {
+        var cell = row.insertCell(i);
+        cell.innerHTML = inner_html_list[i];
+        cell.classList.add(main_class);
+        cell.classList.add(class_list[i]);
+    }
+}

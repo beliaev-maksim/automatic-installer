ebd4b1469bf7b785317d9d0cff403f1f915fb656
Maksim Beliaev <beliaev.m.s@gmail.com>
2020-05-27 20:42:25 +0200

    1. moved completely to JS backend

--- electron_ui/js/main.js
+++ electron_ui/js/main.js
@@ -33,7 +33,7 @@ window.onload = function() {
      * If exists => read settings and populate UI data from the file
      * If not => create default file with settings and dump it to settings file. Then use these settings for UI.
      * 
-     * Since python is started from communicator then send to pyshell request to get active schtasks.
+     * Gets active schtasks.
      * Runs function to set tooltips text
      */
 
@@ -75,24 +75,11 @@ window.onload = function() {
 
     set_selector("artifactory", Object.keys(artifactory_dict), settings.artifactory);
 
-    //pyshell.send('get_active_tasks');
+    get_active_tasks();
     request_builds();
     set_default_tooltips_main();
 }
 
-
-window.onbeforeunload = function(){
-    /**
-     * Before we quit the page we need to shutdown Python process. Otherwise we will spawn it every page load
-    */
-    pyshell.send('exit');
-    pyshell.end(function (err,code,signal) {
-        if (err) throw err;
-        console.log('The exit code was: ' + code);
-        console.log('The exit signal was: ' + signal);
-        console.log('finished');
-    });
-}
 
 
 function get_previous_edt_path() {
@@ -275,7 +262,7 @@ $("#schedule-button").click(function (){
      * If version is empty or not equal to drop menu => server is not grabbed yet => return
      * 
      * Make copy of settings file to another file for scheduling
-     * If all checks are fine then send command to python to set a task and retrieve a new tasks list
+     * If all checks are fine then set a task and retrieve a new tasks list
      */
     if(settings.days.length == 0){
         alert("At least one day should be selected!");
@@ -287,10 +274,10 @@ $("#schedule-button").click(function (){
         fs.copyFileSync(settings_path, scheduled_settings, (err) => {
             if (err) throw err;
         });
-        pyshell.send('schedule_task ' + scheduled_settings);
+        schedule_task(scheduled_settings);
 
         setTimeout(() => {
-            pyshell.send('get_active_tasks');
+            get_active_tasks();
         }, 1000);
         
     } else {
@@ -304,14 +291,13 @@ $("#install-once-button").click(function (){
      * If version is empty or not equal to drop menu => server is not grabbed yet => return
      * 
      * Make copy of settings file to another file for installing once
-     * If all checks are fine then send command to python to install edt
      */
     if(settings.version == $("#version")[0].value && settings.version){
         var install_once_settings = os_path.join(app_folder, "install_once_settings_" + settings.version + ".json");
         fs.copyFileSync(settings_path, install_once_settings, (err) => {
             if (err) throw err;
         });
-        pyshell.send('install_once ' + install_once_settings);   
+        install_once(install_once_settings); 
         alert("Installation started! You can check the progress on Installation History page")     
     } else {
         alert("Version does not exist on artifactory");
---
+++ electron_ui/js/tasks_handler.js
@@ -0,0 +1,85 @@
+var path = require('path');
+const { remote } = require('electron');
+const { app, dialog } = remote;
+const { exec, execSync} = require('child_process');
+var parser = require('fast-xml-parser');
+
+if (app.isPackaged) {
+      backend_exe = path.join(app.getAppPath(), "downloader_backend.exe")  // production
+} else {
+      backend_exe = path.join(app.getAppPath(), "python_build", "downloader_backend.exe");  // develop
+}
+
+function get_task_details(task) {
+      let task_data_dict = {}
+
+      clean_task = task.replace(/(\r\n|\n|\r)/gm,""); //make it single line
+      clean_task = clean_task.replace(/(^\s*<!--.*?-->)/g,""); //Remove comments
+
+      if( parser.validate(clean_task) === true) {
+            var jsonObj = parser.parse(clean_task, { ignoreNameSpace : false });
+
+            let long_days = Object.keys(jsonObj.Task.Triggers.CalendarTrigger.ScheduleByWeek.DaysOfWeek);
+            let short_days = [];
+            for (let i in long_days){
+                  short_days.push(long_days[i].slice(0, 2));
+            }        
+            task_data_dict.days = short_days;
+
+            task_data_dict.time = jsonObj.Task.Triggers.CalendarTrigger.StartBoundary.split("T")[1].slice(0, -3);
+            
+            let task_name = jsonObj.Task.RegistrationInfo.URI.split("\\")[2].split("_");           
+            task_data_dict.version = task_name[0];
+            task_data_dict.product = task_name[1];
+      } else {
+            console.log("failed validation");
+      }
+      return task_data_dict;
+}
+
+function get_active_tasks() {
+      let all_tasks = execSync('schtasks /query /xml').toString();
+      all_tasks = all_tasks.split("\r\n\r\n\r\n");
+      let ansys_tasks = [];
+
+      for(var i = 0; i < all_tasks.length; i++){
+            if (all_tasks[i].includes("AnsysDownloader")){
+                  task_data_dict = get_task_details(all_tasks[i]);
+                  task_data_dict["schedule_time"] = (task_data_dict["days"]).join(", ") + " at " + task_data_dict["time"];
+                  ansys_tasks.push(task_data_dict);
+            }
+      }
+      add_task_rows(ansys_tasks);
+}
+
+function delete_task(task_name) {
+      command = `schtasks /DELETE /TN "AnsysDownloader\\${task_name}" /f`;  // /f - silent
+      execSync(command);
+}
+    
+function schedule_task(settings_file) {
+      unpack_days = {
+            "mo": "MON",
+            "tu": "TUE",
+            "we": "WED",
+            "th": "THU",
+            "fr": "FRI",
+            "sa": "SAT",
+            "su": "SUN"
+      }
+      days = [];
+      for (let i in settings["days"]) {
+            short_day = settings["days"][i];
+            days.push(unpack_days[short_day]);
+      }
+
+      command = (`schtasks /CREATE /TN "AnsysDownloader\\${settings.version}" ` +
+                  `/TR "${backend_exe} -p ${settings_file}" /d ${days.join(",")} /sc WEEKLY /st ${settings["time"]} /f`)
+
+      execSync(command);
+}
+
+function install_once(settings_file) {
+      command = `${backend_exe} -p "${settings_file}"`
+      exec(command);  // async
+}
